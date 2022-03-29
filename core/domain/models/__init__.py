c63934e944f2ab55d07ac36f5f47e7463e3dacbb
Boris Bredikhin <boris00043@yandex.ru>
2022-03-29 16:24:41 +0500

    Модели пользователь, пост, подписка

--- core/domain/models/__init__.py
+++ core/domain/models/__init__.py
@@ -1,10 +1,43 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 
 from infrastructure.database import Base
 
 class Source(Base):
-    __tablename__ = "users"
+    __tablename__ = "source"
 
     id = Column(Integer, primary_key=True, index=True)
     label = Column(String)
     url = Column(String)
+
+    users = relationship('Subscription', back_populates='source')
+
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True, index=True)
+    source_id = Column(Integer(), ForeignKey(Source.id))
+    
+    source = relationship('Source', foreign_keys='Post.source_id')
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nickname = Column(String)
+    password = Column(String)
+
+    sources = relationship('Subscription', back_populates='user')
+
+
+class Subscription(Base):
+    __tablename__ = 'subscription'
+
+    user_id = Column(ForeignKey('user.id'), primary_key=True)
+    source_id = Column(ForeignKey('source.id'), primary_key=True)
+    subscribed_at = Column(DateTime)
+
+    user = relationship('User', back_populates='sources')
+    source = relationship('Source', back_populates='users')
